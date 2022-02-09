50055b508eef9a53d3a74ffadbdab2e0d1608198
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-02-09 01:21:58 -0300

    exercicios de fixaçao secao_8

--- secao_8/exercicios_de_fixacao/ex05.py
+++ secao_8/exercicios_de_fixacao/ex05.py
@@ -1,8 +1,10 @@
 import math
 
+
 def calcula_volume(raio):
     # V= 4/3*pi*r^3
-    volume = 4/3 * math.pi * math.pow(float(raio),3)
-    return (f'O volume da esfera passada é de {volume}')
+    volume = 4 / 3 * math.pi * math.pow(float(raio), 3)
+    return f"O volume da esfera passada é de {volume}"
+
 
 print(calcula_volume(2))
--- secao_8/exercicios_de_fixacao/ex06.py
+++ secao_8/exercicios_de_fixacao/ex06.py
@@ -2,6 +2,7 @@ def converte_em_segundos(horas, minutos, segundos):
     horas_convertidas = horas * 3600
     minutos_convertidos = minutos * 60
     segundos_totais = horas_convertidas + minutos_convertidos + segundos
-    return (f'O total de segundos das horas que você passou é de {segundos_totais}')
+    return f"O total de segundos das horas que você passou é de {segundos_totais}"
+
 
 print(converte_em_segundos(2, 3, 12))
